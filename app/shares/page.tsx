98f517877232cf45ae88808bd7617a8b469ca032
khalifa47 <khalifafumo5@gmail.com>
2023-04-05 15:20:36 +0300

    shares transactions

--- app/shares/page.tsx
+++ app/shares/page.tsx
@@ -1,8 +1,95 @@
 import Title from "@/app/(components)/Title";
+import TransactionsTable from "@/app/(components)/TransactionsTable";
+import { createTransactionData } from "@/utils/helpers";
+import type { GridRowsProp } from "@mui/x-data-grid";
 import dynamic from "next/dynamic";
 
 const InfoCard = dynamic(() => import("@/app/(components)/InfoCard"));
 const Trend = dynamic(() => import("@/app/(components)/Trend"));
+
+const rows: GridRowsProp = [
+  createTransactionData({
+    id: "281936183",
+    amount: 200000,
+    balance: 200000,
+    content: "shares",
+    type: "debit",
+    dateTime: "2004-10-19 10:23:54",
+  }),
+  createTransactionData({
+    id: "282936183",
+    amount: 200000,
+    balance: 200000,
+    content: "shares",
+    type: "debit",
+    dateTime: "2004-10-19 10:23:54",
+  }),
+  createTransactionData({
+    id: "283936183",
+    amount: 200000,
+    balance: 200000,
+    content: "shares",
+    type: "debit",
+    dateTime: "2004-10-19 10:23:54",
+  }),
+  createTransactionData({
+    id: "284936183",
+    amount: 200000,
+    balance: 200000,
+    content: "shares",
+    type: "debit",
+    dateTime: "2004-10-19 10:23:54",
+  }),
+  createTransactionData({
+    id: "285936183",
+    amount: 200000,
+    balance: 200000,
+    content: "shares",
+    type: "debit",
+    dateTime: "2004-10-19 10:23:54",
+  }),
+
+  createTransactionData({
+    id: "81936183",
+    amount: 200000,
+    balance: 200000,
+    content: "shares",
+    type: "debit",
+    dateTime: "2004-10-19 10:23:54",
+  }),
+  createTransactionData({
+    id: "28293183",
+    amount: 200000,
+    balance: 200000,
+    content: "shares",
+    type: "debit",
+    dateTime: "2004-10-19 10:23:54",
+  }),
+  createTransactionData({
+    id: "28393618",
+    amount: 200000,
+    balance: 200000,
+    content: "shares",
+    type: "debit",
+    dateTime: "2004-10-19 10:23:54",
+  }),
+  createTransactionData({
+    id: "24936183",
+    amount: 200000,
+    balance: 200000,
+    content: "shares",
+    type: "debit",
+    dateTime: "2004-10-19 10:23:54",
+  }),
+  createTransactionData({
+    id: "28593683",
+    amount: 200000,
+    balance: 200000,
+    content: "shares",
+    type: "debit",
+    dateTime: "2004-10-19 10:23:54",
+  }),
+];
 
 export default function Shares() {
   return (
@@ -44,6 +131,12 @@ export default function Shares() {
           ]}
         />
       </div>
+
+      <hr style={{ marginTop: 25, marginBottom: 15 }} />
+
+      {/* Shares Transactions */}
+      <Title title="Shares Transactions" />
+      <TransactionsTable rows={rows} content="shares" />
 
       {/* Deposit shares */}
       {/* Withdraw shares */}
