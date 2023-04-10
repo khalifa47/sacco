b84d09e5c3edc55e83a58916f09628703cbdb959
khalifa47 <khalifafumo5@gmail.com>
2023-04-10 20:18:29 +0300

    dialog dimensions set

--- app/(components)/action/ActionDialog.tsx
+++ app/(components)/action/ActionDialog.tsx
@@ -1,6 +1,24 @@
 "use client";
 
 import Dialog from "@mui/material/Dialog";
+import Slide from "@mui/material/Slide";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+
+import { TransitionProps } from "@mui/material/transitions";
+import { forwardRef } from "react";
+import { capitalize } from "@/utils/helpers";
+
+const Transition = forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const ActionDialog = ({
   action,
@@ -12,8 +30,18 @@ const ActionDialog = ({
   handleClose: () => void;
 }) => {
   return (
-    <Dialog open={open} onClose={handleClose}>
-      {action}
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      fullWidth
+      maxWidth="md"
+      TransitionComponent={Transition}
+    >
+      <DialogTitle>{capitalize(action)}</DialogTitle>
+      <DialogContent>{/* dialog content here */}</DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+      </DialogActions>
     </Dialog>
   );
 };
