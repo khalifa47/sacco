c92f12939692b97c0c1b486605de2e7c0c851f9f
khalifa47 <khalifafumo5@gmail.com>
2023-04-11 15:38:28 +0300

    set up formik MUI and action content

---
+++ app/(components)/action/ActionContent.tsx
@@ -0,0 +1,25 @@
+// import * as yup from "yup";
+// import { Formik, Form, Field } from "formik";
+
+// shares
+export const DepositShares = () => {};
+
+export const WithdrawShares = () => {};
+
+export const TransferShares = () => {};
+
+export const SettingShares = () => {};
+
+// loans
+export const HistoryLoans = () => {};
+
+export const RequestLoans = () => {};
+
+export const RepayLoans = () => {};
+
+export const SettingLoans = () => {};
+
+// welfare
+export const DepositWelfare = () => {};
+
+export const SettingWelfare = () => {};
