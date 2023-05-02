6222b2756be43c9ecd226fe94623c015bc030123
khalifa47 <khalifafumo5@gmail.com>
2023-05-02 18:18:11 +0300

    wrap app in supabase context provider

--- app/(components)/layout/Providers.tsx
+++ app/(components)/layout/Providers.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { createContext, useContext, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import type { SupabaseClient } from "@supabase/auth-helpers-nextjs";
+
 import { ThemeProvider, createTheme } from "@mui/material";
 import Container from "@mui/material/Container";
 
@@ -11,12 +17,41 @@ const theme = createTheme({
   },
 });
 
+const Context = createContext<SupabaseClient | undefined>(undefined);
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+  const router = useRouter();
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(() => {
+      router.refresh();
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [router, supabase]);
+
   return (
-    <ThemeProvider theme={theme}>
-      <Container maxWidth="xl">{children}</Container>
-    </ThemeProvider>
+    <Context.Provider value={supabase}>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="xl">{children}</Container>
+      </ThemeProvider>
+    </Context.Provider>
   );
+};
+
+export const useSupabase = () => {
+  const context = useContext(Context);
+
+  if (context === undefined) {
+    throw new Error("useSupabase must be used inside SupabaseProvider");
+  }
+
+  return context;
 };
 
 export default Providers;
