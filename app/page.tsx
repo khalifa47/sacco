a9336b8cb42bce6373af94be9781f17cc639a4c6
khalifa47 <khalifafumo5@gmail.com>
2023-05-05 13:45:22 +0300

    modded login page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { useSupabase } from "./(components)/layout/Providers";
 import { useTheme } from "@mui/material/styles";
 import Skeleton from "@mui/material/Skeleton";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 export default function LoginRegister() {
   const supabase = useSupabase();
@@ -21,32 +23,76 @@ export default function LoginRegister() {
   }, []);
 
   return (
-    <main>
+    <main
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "98vh",
+      }}
+    >
       {isLoading ? (
         <Skeleton variant="rounded" width="100%" height={300} />
       ) : (
-        <Auth
-          supabaseClient={supabase}
-          providers={[]}
-          redirectTo="http://localhost:3000/dashboard"
-          appearance={{
-            theme: ThemeSupa,
-            variables: {
-              default: {
-                colors: {
-                  brand: theme.palette.primary.main,
-                  brandAccent: theme.palette.primary.dark,
+        <Box
+          display="flex"
+          flexDirection={{ xs: "column", md: "row" }}
+          gap={2}
+          width={{ xs: "100%", md: "60%" }}
+        >
+          <Box
+            display="flex"
+            alignItems={"center"}
+            justifyContent={"center"}
+            width={{ xs: "100%", md: "40%" }}
+            my={0}
+            py={2}
+            sx={{
+              background: `linear-gradient(-45deg, ${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
+              borderRadius: 1,
+            }}
+          >
+            <Typography
+              variant="h3"
+              mx={2}
+              flexWrap="wrap"
+              textAlign="center"
+              color="white"
+              fontWeight={700}
+              letterSpacing={2}
+              sx={{
+                textShadow: "2px 2px 4px #000000",
+                transform: { xs: "none", md: "rotate(-45deg)" },
+              }}
+            >
+              KADHI SACCO
+            </Typography>
+          </Box>
+          <Box alignItems="center" width={{ xs: "100%", md: "60%" }}>
+            <Auth
+              supabaseClient={supabase}
+              providers={[]}
+              redirectTo="/dashboard"
+              appearance={{
+                theme: ThemeSupa,
+                variables: {
+                  default: {
+                    colors: {
+                      brand: theme.palette.primary.main,
+                      brandAccent: theme.palette.primary.dark,
+                    },
+                    fonts: {
+                      bodyFontFamily: theme.typography.fontFamily,
+                      buttonFontFamily: theme.typography.fontFamily,
+                      inputFontFamily: theme.typography.fontFamily,
+                      labelFontFamily: theme.typography.fontFamily,
+                    },
+                  },
                 },
-                fonts: {
-                  bodyFontFamily: theme.typography.fontFamily,
-                  buttonFontFamily: theme.typography.fontFamily,
-                  inputFontFamily: theme.typography.fontFamily,
-                  labelFontFamily: theme.typography.fontFamily,
-                },
-              },
-            },
-          }}
-        />
+              }}
+            />
+          </Box>
+        </Box>
       )}
     </main>
   );
