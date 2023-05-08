4a4d5f86cfe9cb78846dd215d580765e4a62fb83
khalifa47 <khalifafumo5@gmail.com>
2023-05-08 15:58:35 +0300

    margin 0 on body

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body style={{ margin: 0 }}>
         <Providers>{children}</Providers>
       </body>
     </html>
