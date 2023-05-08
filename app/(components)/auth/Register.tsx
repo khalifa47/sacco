6aebfafcee5d928d1ac4444127419b04550a668c
khalifa47 <khalifafumo5@gmail.com>
2023-05-08 18:53:39 +0300

    register form init

---
+++ app/(components)/auth/Register.tsx
@@ -0,0 +1,12 @@
+import { Formik, Form, Field } from "formik";
+import CircularProgress from "@mui/material/CircularProgress";
+import { TextField } from "formik-mui";
+import Button from "@mui/material/Button";
+
+import type { SupabaseClient } from "@supabase/supabase-js";
+
+const Register = ({ supabase }: { supabase: SupabaseClient }) => {
+  return <div>Register</div>;
+};
+
+export default Register;
