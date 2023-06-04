deb66b09cc0ffa7b8d98c8c567a160313ed4dbfa
khalifa47 <khalifafumo5@gmail.com>
2023-06-04 15:36:56 +0300

    welfare data fetch

--- app/(pages)/welfare/page.tsx
+++ app/(pages)/welfare/page.tsx
@@ -2,13 +2,40 @@ import Title from "@/app/(components)/layout/Title";
 import DataTable from "@/app/(components)/data/DataTable";
 import type { GridRowsProp } from "@mui/x-data-grid";
 import dynamic from "next/dynamic";
+import { headers, cookies } from "next/headers";
 import Divider from "@/app/(components)/layout/Divider";
 import Actions from "@/app/(components)/action/Actions";
+import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { getContributionAmount, getTransactionData } from "@/utils/fetchers";
+import { ContributionTransaction } from "@prisma/client";
+import { groupTransactionsByMonth } from "@/utils/helpers";
 
 const InfoCard = dynamic(() => import("@/app/(components)/data/InfoCard"));
 const Trend = dynamic(() => import("@/app/(components)/data/Trend"));
 
-export default function Welfare() {
+export default async function Welfare() {
+  const supabase = createServerComponentSupabaseClient({
+    headers,
+    cookies,
+  });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session === null) {
+    throw new Error("User not authenticated");
+  }
+
+  const contributionAmountData = getContributionAmount(session.user.id);
+  const transactionsData: Promise<GridRowsProp<ContributionTransaction>> =
+    getTransactionData(session.user.id, undefined, "welfare");
+
+  const [{ welfare }, transactions] = await Promise.all([
+    contributionAmountData,
+    transactionsData,
+  ]);
+
   return (
     <main>
       <Title title="My Welfare" pageTitle />
@@ -21,7 +48,7 @@ export default function Welfare() {
           gap: 20,
         }}
       >
-        <InfoCard content="welfare" amount={200000} />
+        <InfoCard content="welfare" amount={welfare} />
         <Trend
           content="welfare"
           labels={[
@@ -40,7 +67,7 @@ export default function Welfare() {
           ]}
           datasets={[
             {
-              data: [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3],
+              data: groupTransactionsByMonth(transactions),
             },
           ]}
         />
@@ -50,7 +77,7 @@ export default function Welfare() {
 
       {/* Welfare Transactions */}
       <Title title="Welfare Transactions" />
-      <DataTable rows={[]} />
+      <DataTable rows={transactions} />
 
       <Divider />
 
