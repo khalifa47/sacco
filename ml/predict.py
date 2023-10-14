12243fce78a2aa79c5a04f2d29c54e2f400bd0d5
khalifa47 <khalifafumo5@gmail.com>
2023-10-14 12:17:44 +0300

    added investment prediction module

--- ml/predict.py
+++ ml/predict.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+import numpy as np
 
-mapping = {
+# Loan Default definitions
+loan_mapping = {
     'all_other': 0,
     'credit_card': 1,
     'debt_consolidation': 2,
@@ -12,16 +14,33 @@ mapping = {
     'small_business': 6
 }
 
-columns_order = [
+loan_columns_order = [
     "credit.policy", "purpose", "installment", "log.annual.inc",
     "dti", "fico", "days.with.cr.line", "revol.bal",
     "revol.util", "inq.last.6mths", "delinq.2yrs", "pub.rec"
 ]
 
-app = Flask(__name__)
+loan_model1 = joblib.load('./loan_default/loan_predict.joblib')
+loan_model2 = joblib.load('./loan_default/loan_predict_resampled.joblib')
 
-model1 = joblib.load('./loan_default/loan_predict.joblib')
-model2 = joblib.load('./loan_default/loan_predict_resampled.joblib')
+# Investment definitions
+
+inv_mapping = {
+    'crypto': 0,
+    'etf': 1,
+    'forex': 2,
+    'index': 3,
+    'metals': 4,
+    'real_estate': 5,
+    'stocks': 6
+}
+
+inv_model = joblib.load('./investment/nmf_model.joblib')
+
+
+# Flask app
+
+app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
@@ -33,19 +52,48 @@ def hello():
 def predict_default_probability():
     try:
         data = request.json['data']
-        if data['purpose'] not in mapping:
+        if data['purpose'] not in loan_mapping:
             data['purpose'] = "all_other"
-        data['purpose'] = mapping[data['purpose']]
+        data['purpose'] = loan_mapping[data['purpose']]
 
-        input_df = pd.DataFrame([data], columns=columns_order)
+        input_df = pd.DataFrame([data], columns=loan_columns_order)
 
-        scaler = joblib.load('./loan_default/scaler.pkl')
-        input_df = scaler.transform(input_df)
+        loan_scaler = joblib.load('./loan_default/scaler.pkl')
+        input_df = loan_scaler.transform(input_df)
 
-        prob_default = (0.4 * model1.predict_proba(input_df)
-                        [:, 1][0]) + (0.6 * model2.predict_proba(input_df)[:, 1][0])
+        prob_default = (0.4 * loan_model1.predict_proba(input_df)
+                        [:, 1][0]) + (0.6 * loan_model2.predict_proba(input_df)[:, 1][0])
 
         return jsonify({'probability_of_default': prob_default})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}, 500)
+
+
+@app.route('/predict_investment', methods=['POST'])
+def predict_investment():
+    try:
+        # user_appetite, user_loan_amount, user_average_loan_risk, user_shares_amount
+        data = request.json['data']
+
+        user_input = np.array([data['user_appetite'], data['user_loan_amount'],
+                              data['user_average_loan_risk'], data['user_shares_amount']])
+
+        inv_scaler = joblib.load('./investment/scaler_nmf.joblib')
+        scaled_input = inv_scaler.transform(user_input.reshape(1, -1))
+
+        predicted_affinities = np.dot(scaled_input, inv_model.components_.T)[0]
+
+        top_indices = np.argsort(predicted_affinities)[
+            ::-1]  # Sort in descending order
+
+        label_to_investment_type = {v: k for k, v in inv_mapping.items()}
+
+        # Get the top 3 recommended investment types and their affinities
+        top_3_recommendations = [
+            (label_to_investment_type[idx], predicted_affinities[idx]) for idx in top_indices[:3]]
+
+        return jsonify({'top3inv': top_3_recommendations})
 
     except Exception as e:
         return jsonify({'error': str(e)}, 500)
